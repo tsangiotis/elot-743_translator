902ba281a38e77e500d4a2310434a3ac401aa54a
Tasos Sagiotis <tsagiotis@gmail.com>
2013-05-18 20:39:57 +0300

    initial common and README update

--- README.md
+++ README.md
@@ -2,3 +2,15 @@ elot-743_translator
 ===================
 
 A Python script to translate Greek Names to English according to ELOT-743
+
+##Instructions
+
+To execute:
+
+```
+python names.py -i <inputfile> -o <outputfile>
+```
+
+***inputfile:*** A text file with the names to be translated.
+
+***outputfile:*** The textfile where the translated names will be written.
---
+++ names.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+
+# -*- coding= utf8 -*-
+import sys, getopt
+from string import maketrans
+from elot743 import *
+
+def replace_all(text, dic):
+	for i, j in dic.iteritems():
+    		if i != j:
+        		text = text.replace(i, j)
+	text.replace(',' , '\n')
+	return text
+
+def readFile(infile):
+	print "reading file",infile,"...\n"
+	i = open(infile, 'r')
+	array=[]
+	for line in i:
+		array.append(replace_all(line.rstrip('\n'),dict))
+
+	i.close()
+	return array
+	print "\nfinished reading file!\n"
+
+def writeFile(outfile, array):
+	print "writing to file",outfile,"...\n"
+	print array
+	o = open(outfile, 'w')
+	for item in array:
+ 		o.write("%s\n" % item)
+ 	o.close()
+	print "\nfinished writing to file!\n" 
+
+def main(argv):
+   inputfile = ''
+   outputfile = ''
+   try:
+      opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+   except getopt.GetoptError:
+      print 'names.py -i <inputfile> -o <outputfile>'
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print 'names.py -i <inputfile> -o <outputfile>'
+         sys.exit()
+      elif opt in ("-i", "--ifile"):
+         inputfile = arg
+      elif opt in ("-o", "--ofile"):
+         outputfile = arg
+   print 'Input file is "', inputfile
+   print 'Output file is "', outputfile
+
+   transArray=[]
+
+   transArray=readFile(inputfile)
+   writeFile(outputfile,transArray)
+   print 'File is translated!'
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
